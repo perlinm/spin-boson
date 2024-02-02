a187ed75d9d97f58eab68ed099be817704431bb5
Michael A. Perlin <mika.perlin@gmail.com>
2024-02-02 08:16:10 -0600

    reduce parameter search space

--- run_sims.sh
+++ run_sims.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 
 states=$(for i in $(seq 1 20); do echo -n "dicke-$i "; done)
-decay=(0.2 0.4 0.6 0.8 1.0 1.2 1.4 1.6 1.8 2.0 2.2 2.4 2.6 2.8 3.0)
+decay=(0 0.2 0.4 0.6 0.8 1.0)
 
 for num_spins in {5..20}; do
   echo -------------------------
