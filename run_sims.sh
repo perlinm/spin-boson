ca422f4bd3b4e92a1b58b0ca66936997d04b6de4
Michael A. Perlin <mika.perlin@gmail.com>
2024-02-06 16:45:44 -0600

    nit changes to runner script

--- run_sims.sh
+++ run_sims.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env zsh
 
-states=$(for i in $(seq 1 20); do echo -n "dicke-$i "; done)
 decay=(0 0.2 0.4 0.6 0.8 1.0)
 
-for num_spins in {5..20}; do
+for num_spins in {5..15}; do
   echo -------------------------
   echo $num_spins
   echo -------------------------
-  for state in ghz x-polarized $(for i in {1..$num_spins}; do echo -n "dicke-$i "; done); do
+  for state in ghz x-polarized $(for ii in {1..$num_spins}; do echo "dicke-$ii"; done); do
     python3 collect_data.py --num_spins $num_spins --state_keys $state --decay $decay
     python3 collect_data.py --num_spins $num_spins --state_keys $state --decay $decay --dephasing
   done
